fe986eb0388aaea498a7338909d1682ca6566397
ivan schlagheck <ivansch05@gmail.com>
2021-11-12 17:52:04 +0100

    i try to do the root thing

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -14,7 +14,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to root_path(@list)
    else
       render :new
     end
@@ -23,7 +23,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    redirect_to root_path(@bookmark.list)
   end
 
   private
